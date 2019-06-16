bc9dfd8fae6731fefd7332805a231cee1fe3844e
Daniel Rodriguez <drodriguez661@gmail.com>
2019-06-16 06:26:02 -0500

    added some js

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+GAME FUNCTION:
+- PLAYER MUST GUESS A NUMBER BETWEEN A MIN AND MAX
+-PLAYER GETS A CERTAIN AMOUNT OF GUESSES
+-NOTIFY PLAYER OF GUESSES REMAINING
+-LET PLAYER CHOOSE TO PLAY AGAIN
+*/
+
+// Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+//ui elements
+const game = doccumentGetElementById('game'),
